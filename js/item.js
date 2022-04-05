7440f2c142ca92fee2362c0bb44bad3ddeeead5f
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2022-04-05 14:24:50 +0430

    item tabs implemented

--- js/item.js
+++ js/item.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const headerContainerEl = document.querySelector(".header > .container");
+const tabsHeaderThs = document.getElementsByClassName("tabs-header__th");
 
 window.addEventListener("scroll", function () {
   let yAxis = window.scrollY;
@@ -12,3 +13,22 @@ window.addEventListener("scroll", function () {
     headerContainerEl.style.background = "transparent";
   }
 });
+
+for (const [i, item] of Object.entries(tabsHeaderThs)) {
+  tabsHeaderThs[i].addEventListener("click", function () {
+    for (const x of tabsHeaderThs) {
+      x.classList.remove("tabs-header__th--active");
+    }
+    document
+      .querySelector(".item-tabs__item.active-x")
+      .classList.remove("active-x");
+    this.classList.add("tabs-header__th--active");
+    const tempClassName =
+      this.textContent.toLowerCase() === "bid history"
+        ? "history"
+        : this.textContent.toLowerCase();
+    document
+      .querySelector(`.item-tabs__${tempClassName}`)
+      .classList.add("active-x");
+  });
+}
